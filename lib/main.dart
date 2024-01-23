b5510b0fabb4cd80ab7631bf2a380656b4e34315
mt1nr <schafi350@gmail.com>
2024-01-23 07:21:51 +0100

    struktur
    wem es nicht passt soll was besseres vorschlagen (beschwerdenflo)

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'authentication_screens/Login.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
 
@@ -41,7 +40,6 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: Login(),
     );
   }
 }
