b69570402486b0ec37dfb711409d5477f033ce4f
Florian Kreher <76181281+Flohkiste@users.noreply.github.com>
2024-04-21 19:28:10 +0200

    Update orders_screen.dart
    wenn bestellung geschlossen wird dann wird jetzt auch plus gerechnet jaaaa
    und ich hab mal die methoden sotrtiert und fucking überschriften mit Kommentaren gemacht was ist den hier los

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -6,30 +6,41 @@ import 'package:shared_preferences/shared_preferences.dart';
 
 import '../values/app_strings.dart';
 
-enum States { OPEN, CLOSED }
+enum States { open, closed }
 
 class OrderScreen extends StatefulWidget {
   final String tableNum;
 
-  OrderScreen({required this.tableNum});
+  const OrderScreen({super.key, required this.tableNum});
 
   @override
   _OrderScreenState createState() => _OrderScreenState();
 }
 
 class _OrderScreenState extends State<OrderScreen> {
-  States curState = States.OPEN;
+  States curState = States.open;
   Map<String, String> orders = {};
   Map<String, String> dishNames = {};
   String? restaurantId;
   DatabaseReference? ref;
+  StreamSubscription<DatabaseEvent>? _subscription;
 
+
+  //Overrides
   @override
   void initState() {
     super.initState();
     setupAsync();
   }
 
+  @override
+  void dispose() {
+    _subscription?.cancel();
+    super.dispose();
+  }
+
+
+  //Setup
   Future<void> setupAsync() async {
     await loadRestaurantId();
     loadDishNames();
@@ -43,19 +54,21 @@ class _OrderScreenState extends State<OrderScreen> {
     }
   }
 
+
+  //Widgets
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(AppStrings.table + " " + widget.tableNum),
+        title: Text("${AppStrings.table} ${widget.tableNum}"),
       ),
       body: Column(
         children: <Widget>[
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: <Widget>[
-              _buildButton('Open Orders', States.OPEN),
-              _buildButton('Closed Orders', States.CLOSED),
+              _buildButton('Open Orders', States.open),
+              _buildButton('Closed Orders', States.closed),
             ],
           ),
           Expanded(
@@ -63,7 +76,7 @@ class _OrderScreenState extends State<OrderScreen> {
               crossAxisCount: 1,
               childAspectRatio: 8 / 2,
               children:
-              orders.keys.map((item) => _buildSquareButton(item)).toList(),
+                  orders.keys.map((item) => _buildSquareButton(item)).toList(),
             ),
           ),
         ],
@@ -79,11 +92,11 @@ class _OrderScreenState extends State<OrderScreen> {
           updateData();
         });
       },
-      child: Text(title, style: TextStyle(color: Colors.black)),
       style: ButtonStyle(
         backgroundColor: MaterialStateProperty.all<Color>(
             curState == state ? Colors.amber : Colors.grey),
       ),
+      child: Text(title, style: const TextStyle(color: Colors.black)),
     );
   }
 
@@ -93,43 +106,45 @@ class _OrderScreenState extends State<OrderScreen> {
         borderRadius: BorderRadius.circular(10),
         border: Border.all(color: Colors.black, width: 2),
       ),
-      margin: EdgeInsets.all(8.0),
+      margin: const EdgeInsets.all(8.0),
       child: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
           Padding(
-            padding: EdgeInsets.only(left: 20.0),
+            padding: const EdgeInsets.only(left: 20.0),
             child: Row(
               children: [
                 Text(
-                  (orders[item] != null ? orders[item]! : '0') +
-                      "x " +
-                      (dishNames[item] != null ? dishNames[item]! : 'Unknown'),
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 22),
+                  "${orders[item] != null ? orders[item]! : '0'}x ${dishNames[item] != null ? dishNames[item]! : 'Unknown'}",
+                  style: const TextStyle(
+                      fontWeight: FontWeight.bold, fontSize: 22),
                 ),
                 Expanded(
                   child: Row(
                     children: [
-                      Spacer(),
+                      const Spacer(),
                       Padding(
-                        padding: EdgeInsets.only(right: 20.0),
+                        padding: const EdgeInsets.only(right: 20.0),
                         child: Padding(
-                          padding: EdgeInsets.all(10.0),
+                          padding: const EdgeInsets.all(10.0),
                           child: TextButton(
                             onPressed: () => closeOpenOrder(item),
+                            style: ButtonStyle(
+                              backgroundColor:
+                                  MaterialStateProperty.all<Color?>(
+                                      curState == States.open
+                                          ? Colors.amber
+                                          : Colors.amber),
+                            ),
                             child: Padding(
-                              padding: EdgeInsets.all(10.0),
+                              padding: const EdgeInsets.all(10.0),
                               child: Text(
-                                curState == States.CLOSED
+                                curState == States.closed
                                     ? "Reopen Order"
                                     : "Close Order",
-                                style: TextStyle(color: Colors.black),
+                                style: const TextStyle(color: Colors.black),
                               ),
-                            ),
-                            style: ButtonStyle(
-                              backgroundColor: MaterialStateProperty.all<Color?>(
-                                  curState == States.OPEN ? Colors.amber : Colors.amber),
                             ),
                           ),
                         ),
@@ -145,90 +160,23 @@ class _OrderScreenState extends State<OrderScreen> {
     );
   }
 
-  Future<void> closeOpenOrder(var dish) async {
-    String formattedTableNum = 'T' + widget.tableNum.padLeft(3, '0');
-    DatabaseReference ref = FirebaseDatabase.instance.ref(
-        'Restaurants/$restaurantId/tische/$formattedTableNum');
-    if (curState == States.OPEN) {
-      await ref.update(
-          {
-            "geschlosseneBestellungen/$dish": int.parse(orders[dish]!),
-            "bestellungen/$dish": 0
-          }
-      );
-    } else {
-      await ref.update(
-          {
-            "bestellungen/$dish": int.parse(orders[dish]!),
-            "geschlosseneBestellungen/$dish": 0
-          }
-      );
-    }
 
-    print(curState);
-    updateData();
-
-  }
-
-  StreamSubscription<DatabaseEvent>? _subscription;
-
+  //Database
   void setupFirebase() {
-    String formattedTableNum = 'T' + widget.tableNum.padLeft(3, '0');
-    if (curState == States.OPEN) {
-      ref = FirebaseDatabase.instance.ref(
-          'Restaurants/$restaurantId/tische/$formattedTableNum/bestellungen');
-    } else {
-      ref = FirebaseDatabase.instance.ref(
-          'Restaurants/$restaurantId/tische/$formattedTableNum/geschlosseneBestellungen');
-    }
+    String formattedTableNum = 'T${widget.tableNum.padLeft(3, '0')}';
+    ref = FirebaseDatabase.instance.ref('Restaurants/$restaurantId/tische/$formattedTableNum');
+
 
     _subscription = ref!.onValue.listen((event) {
-      var snapshot = event.snapshot;
-      if (snapshot.value != null) {
-        if (snapshot.value is Map<dynamic, dynamic>) {
-          Map<dynamic, dynamic> values =
-          snapshot.value as Map<dynamic, dynamic>;
+      var snapshot = event.snapshot.child(curState == States.open? 'bestellungen':'geschlosseneBestellungen');
 
-          // Convert the Map<dynamic, dynamic> to Map<String, String>
-          Map<String, String> stringMap = values.map(
-                  (key, value) => MapEntry(key.toString(), value.toString()));
-
-          // Filter out entries with 0 orders
-          stringMap.removeWhere((key, value) => value == '0');
-
-          // Sort the entries based on the keys
-          var entries = stringMap.entries.toList()
-            ..sort((a, b) => a.key.compareTo(b.key));
-
-          // Convert the sorted entries back to a map and assign it to orders
-          if (mounted) {
-            setState(() {
-              orders = Map.fromEntries(entries);
-            });
-          }
-        }
-      }
+      setState(() {
+        orders = getData(snapshot);
+      });
     });
   }
 
-  @override
-  void dispose() {
-    _subscription?.cancel();
-    super.dispose();
-  }
-
-  void updateData() async {
-    String formattedTableNum = 'T' + widget.tableNum.padLeft(3, '0');
-    if (curState == States.OPEN) {
-      ref = FirebaseDatabase.instance.ref(
-          'Restaurants/$restaurantId/tische/$formattedTableNum/bestellungen');
-    } else {
-      ref = FirebaseDatabase.instance.ref(
-          'Restaurants/$restaurantId/tische/$formattedTableNum/geschlosseneBestellungen');
-    }
-
-    final snapshot = await ref?.get();
-
+  Map<String, String> getData(snapshot) {
     if (snapshot?.value != null) {
       if (snapshot?.value is Map<dynamic, dynamic>) {
         Map<dynamic, dynamic> values = snapshot?.value as Map<dynamic, dynamic>;
@@ -243,18 +191,60 @@ class _OrderScreenState extends State<OrderScreen> {
         var entries = stringMap.entries.toList()
           ..sort((a, b) => a.key.compareTo(b.key));
 
-        // Convert the sorted entries back to a map and assign it to orders
-        setState(() {
-          orders = Map.fromEntries(entries);
-        });
+        return Map.fromEntries(entries);
       }
     }
+    return {};
+  }
+
+  Future<void> closeOpenOrder(var dish) async {
+    String formattedTableNum = 'T${widget.tableNum.padLeft(3, '0')}';
+    DatabaseReference ref = FirebaseDatabase.instance
+        .ref('Restaurants/$restaurantId/tische/$formattedTableNum');
+    if (curState == States.open) {
+
+      final snapshot = await ref?.child("geschlosseneBestellungen").get();
+      Map<String, String> closedOrders = getData(snapshot);
+
+      await ref.update({
+        "geschlosseneBestellungen/$dish": int.parse(closedOrders[dish] ?? '0') + int.parse(orders[dish] ?? '0'),
+        "bestellungen/$dish": 0
+      });
+    } else {
+
+      final snapshot = await ref?.child("bestellungen").get();
+      Map<String, String> openOrders = getData(snapshot);
+
+      await ref.update({
+        "bestellungen/$dish": int.parse(openOrders[dish] ?? '0') + int.parse(orders[dish] ?? '0'),
+        "geschlosseneBestellungen/$dish": 0
+      });
+    }
+
+    updateData();
+  }
+
+  void updateData() async {
+    String formattedTableNum = 'T${widget.tableNum.padLeft(3, '0')}';
+    if (curState == States.open) {
+      ref = FirebaseDatabase.instance.ref(
+          'Restaurants/$restaurantId/tische/$formattedTableNum/bestellungen');
+    } else {
+      ref = FirebaseDatabase.instance.ref(
+          'Restaurants/$restaurantId/tische/$formattedTableNum/geschlosseneBestellungen');
+    }
+
+    final snapshot = await ref?.get();
+
+    setState(() {
+      orders = getData(snapshot);
+    });
   }
 
   void loadDishNames() async {
     final ref = FirebaseDatabase.instance.ref();
     final snapshot =
-    await ref.child('Restaurants/$restaurantId/speisekarte').get();
+        await ref.child('Restaurants/$restaurantId/speisekarte').get();
 
     Map<dynamic, dynamic> values = snapshot.value as Map<dynamic, dynamic>;
     for (var entry in values.entries) {
@@ -267,4 +257,6 @@ class _OrderScreenState extends State<OrderScreen> {
       });
     }
   }
+
+
 }
