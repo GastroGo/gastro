878608efc8bce7b45d85806f9f4d35e1a407bed9
Florian Kreher <76181281+Flohkiste@users.noreply.github.com>
2024-04-23 17:14:46 +0200

    test

--- lib/screens/orders_screen.dart
+++ lib/screens/orders_screen.dart
@@ -271,11 +271,11 @@ class _OrderScreenState extends State<OrderScreen> {
         }
         break;
       case States.closed:
-        final snapshotOpen = await ref.child("bestellungen").get();
+        final snapshotOpen = await ref.child("bestellungen").once();
         Map<String, String> openOrders = getData(snapshotOpen);
 
         final snapshotClosed =
-            await ref.child("geschlosseneBestellungen").get();
+            await ref.child("geschlosseneBestellungen").once();
         Map<String, String> closedOrders = getData(snapshotClosed);
 
         await ref.update({
